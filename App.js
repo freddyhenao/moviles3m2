373b9e2c35805a88e16dbf448915783deefaef31
freddyhenao <61300657+freddyhenao@users.noreply.github.com>
2020-11-28 18:15:49 -0500

    Pagina Principal

--- App.js
+++ App.js
@@ -1,125 +1,90 @@
 
+//    import React, {useState} from 'react';
+//    import {Text,View} from 'react-native';
+   
+//    import { NavigationContainer } from '@react-navigation/native';
+//    import { createStackNavigator } from '@react-navigation/stack';
+//    import HomePage from './pages/home-page/home-page';
+//    import ContactPage from './pages/contact-page/contact-page';
+//    import AboutPage from './pages/about-page/about-page';
+
+//  const Stack = createStackNavigator();
+
+//  function App() {
+//   return(
+
+
+//     <NavigationContainer>
+//        <Stack.Navigator>
+//        <Stack.Screen name="Home" component={HomePage}/>
+//        <Stack.Screen name="Contact" component={ContactPage}/>
+//        <Stack.Screen name="About" component={AboutPage}/>
+//        </Stack.Navigator>
+//     </NavigationContainer>
+//   );  
+// }
+// export default App;
+// 
+//*********************** Inicio de aplicacion movilñ React Native ..........  */
 import React from 'react';
 import { 
-  ColorPropType, 
+  View, 
   StyleSheet, 
   Text, 
   Button, 
-  View, 
   Alert, 
   TouchableHighlight, 
   Dimensions,
-  Image } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+  Image} from 'react-native';
 
 const pressMe = () =>{
-  Alert.alert("Peligro");
-}
-const App =() =>{
-  return(
-    <View style={styles.container}>
-      <View style={styles.box10}>
-        <Image source={require('./assets/cita.png')}></Image>
-        <Text style={{fontSize:20, color:"white", fontWeight:"bold"}}>AGENDA DE CITAS MEDICAS </Text>
-      </View>
-      <View style={styles.box11}></View>
-      <View style={styles.box12}></View>
-    </View>
-  );
+  Alert.alert("peligro");
 }
 
-const styles= StyleSheet.create({
-  container:{
+const App =() =>{
+  return(
+    <View style  ={styles.container}>
+        <View style={styles.box1}>
+          <Image source={require('./assets/cita.png')} style={{width:150}}></Image>
+          <Text style={{fontSize:20, color:"black"}}> Agenda de Citas Medicas </Text>
+        </View>
+        <View style={styles.box2}>
+       
+        </View>
+        <View style={styles.box3}>
+        <Text>Agenda de Citas Medicas</Text>
+        </View>
+        
+
+    </View>
+   
+  );
+}
+const styles = StyleSheet.create({
+  container : {
     flex: 1,
-    flexDirection: "column"
-  }, 
+    flexDirection: "column",
+  },
   box1:{
     flex: 1,
-    flexDirection: "row"
+    backgroundColor: "#29b6f6",
+    justifyContent: "center",
+    alignItems: "center"
   },
   box2:{
     flex: 1,
-    backgroundColor: "brown",
-    margin: 20,
-    borderRadius:15,
-    alignItems:"center",
-    justifyContent: "center"
-  },
+    backgroundColor: "#29b6f6",
+    },
   box3:{
-    flex: 1,
-    backgroundColor: "purple",
-    margin: 20,
-    borderRadius:15,
-    alignItems:"center",
-    justifyContent: "center"
-    
-  },
-  box4: {
-    flex: 1,
-    flexDirection: "row"
-  },
-  box5:{
-    flex: 1,
-    backgroundColor: "blue",
-    margin: 20,
-    borderRadius:15,
-    borderWidth: 2,
-    alignItems:"center",
-    justifyContent: "center"
-  },
-  box6:{
-    flex: 1,
-    backgroundColor: "yellow",
-    margin: 20,
-    borderRadius:15
- },
- box7:{
-  flex: 1,
-  flexDirection: "row"
-},
-box8:{
-  flex: 1,
-  backgroundColor: "black",
-  margin: 20,
-  borderRadius:15
-},
-box9:{
-  flex: 1,
-  backgroundColor: "green",
-  margin: 20,
-  borderRadius:15
-},
-text:{
-  color:"white",
-  fontSize: 20
-},
-touchButton:{
-  padding:20,
-  backgroundColor:"#3f51b5",
-  flexDirection:"row",
-  borderRadius: 10
-},
-box10:{
-  flex: 1,
-  backgroundColor: "#03a9f4",
-  alignItems:"center",
-  justifyContent: "center"
-},
-box11:{
-  flex: 1,
-  backgroundColor: "white"
-},
-box12:{
-  width: Dimensions.get("screen").width*0.9,
-  height: Dimensions.get("screen").height*0.5,
-  backgroundColor: "white",
-  position:"absolute",
-  left: Dimensions.get("screen").width*0.05,
-  right: Dimensions.get("screen").width*0.05,
-  top: Dimensions.get("screen").height*0.35,
-  borderRadius: 10,
-}
- 
-})
+    width: Dimensions.get("screen").width*0.9,
+    height: Dimensions.get("screen").height*0.4,
+    backgroundColor: "#b3e5fc",
+    position: "absolute",
+    left: Dimensions.get("screen").width*0.05,
+    right: Dimensions.get("screen").width*0.05,
+    top: Dimensions.get("screen").width*1,
+    borderRadius: 10,
+  }
 
+});
 export default App;
